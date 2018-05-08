3c078974935d133046c342d85e2e6b0ccc3834dd
ZinkPavel <zink.pavel@hotmail.com>
2018-05-08 22:29:53 +0700

    random & enter (take 2)

--- src/dictionary.c
+++ src/dictionary.c
@@ -1,15 +1,11 @@
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "string.h"
 #include "dictionary.h"
+#include "string.h"
 
 int count_string(FILE * input)
 {
 	int lines_count = 0;
 
-	while(!feof(input)) {
+	while( !feof(input)) {
 		if(fgetc(input) == '\n') {
 			lines_count++;
 		}
@@ -19,10 +15,11 @@ int count_string(FILE * input)
 
 }
 
-dictionary *dictionary_init(int max_words_in_dictionary)
+struct dictionary *dictionary_init(int max_words_in_dictionary)
 {
-	dictionary *tab;
-	tab = (dictionary*)malloc(max_words_in_dictionary*sizeof(dictionary));
+	printf("Функция dictionary_init\n");
+	struct dictionary *tab;
+	tab = (struct dictionary*)malloc(max_words_in_dictionary*sizeof(struct dictionary));
 	for (int i = 0; i < max_words_in_dictionary; i++) {
 		tab[i].first_f = (char*)malloc(20*sizeof(char));
 		tab[i].second_f = (char*)malloc(20*sizeof(char));
@@ -47,9 +44,9 @@ dictionary *dictionary_init(int max_words_in_dictionary)
 	return tab;
 }
 
-dictionary *dictionary_reading(dictionary *tab, int max_words_in_dictionary)
+struct dictionary *dictionary_reading(struct dictionary *tab, int max_words_in_dictionary)
 {
-	int i;
+	printf("Функция dictionary_reading\n");
 	FILE *dictionary;
 	dictionary = fopen("dictionary.txt", "r");
 	if(dictionary == NULL) {
@@ -57,79 +54,36 @@ dictionary *dictionary_reading(dictionary *tab, int max_words_in_dictionary)
 		return 0;
 	}
 	char *buffer = (char*)malloc(50*sizeof(char));
-	char  *p[10];
+	char **p;
 
-	for (i = 0; i < max_words_in_dictionary; i++) {
+	printf("max_words_in_dictionary = %d\n", max_words_in_dictionary);
+
+	for (int i = 0; i < max_words_in_dictionary; i++) {
 		fscanf(dictionary, "%s", buffer);
-		s_tok(buffer, ';', p);
+		printf("buffer(%d) = %s\n", i, buffer);
+		p = stok(buffer, ';');
 		scopy(p[0], tab[i].first_f);
 		scopy(p[1], tab[i].second_f);
 		scopy(p[2], tab[i].third_f);
 		scopy(p[3], tab[i].rus);
+		printf("1 form(%d) = %s\n2 form(%d) = %s\n3 form(%d) = %s\nrus(%d) = %s\n", i, tab[i].first_f, i, tab[i].second_f, i, tab[i].third_f, i, tab[i].rus);
 	}
+	stok_free(p);
 	fclose(dictionary);
 	free(buffer);
 
 	return tab;
 }
 
-int random_generator(int max_words_in_dictionary, int value[], int amount)
-{
-	int i;
-	value = realloc(value, sizeof(int)*amount);
 
-	srand(time(NULL));
-	for (i = 0; i < amount; i++) {
-		value[i] = rand() % max_words_in_dictionary;
+void dictionary_clean(struct dictionary *tab, int max_words_in_dictionary)
+{
+	for (int i = 0; i < max_words_in_dictionary; i++) {
+
+		free(tab[i].first_f);
+		free(tab[i].second_f);
+		free(tab[i].third_f);
+		free(tab[i].rus);
 	}
-	return 0;
-}
-
-int random_check(int max_words_in_dictionary, int value[], int amount)
-{
-	int i, j;
-
-	for (i = 0; i < amount; i++) {
-		for (j = 0; j < amount; j++) {
-			if ((value[i] == value[j]) && (i != j)) {
-				do {
-					value[i] = rand() % max_words_in_dictionary;
-				} while (value[i] == value[j]);
-				return random_check(max_words_in_dictionary, value, amount);
-			}
-		}
-	}
-	return 0;
-}
-
-int enter_words(dictionary *tab, int value[], int amount)
-{
-	int i, result;
-	char buffer[100], delim = '/', *part[3];
-
-	for (i = 0; i < amount; i++) {
-		printf("Введите 3 формы слова [%s] через '%c'.\n Enter: ", tab[value[i]].rus, delim);
-		scanf("%s", buffer);
-		printf("\n");
-
-		result = 0;
-		s_tok(buffer, '/', part);
-		if ((s_cmp(tab[value[i]].first_f, part[0]) == 0)) {
-			result++;
-		}
-		if ((s_cmp(tab[value[i]].second_f, part[1]) == 0)) {
-			result++;
-		}
-		if ((s_cmp(tab[value[i]].third_f, part[2]) == 0)) {
-			result++;
-		}
-		printf("Результат = %d из 3 попаданий.\n", result);
-	}
-	return 0;
-}
-
-void dictionary_clean(dictionary *tab, int value[])
-{
 	free(tab);
-	free(value);
 }
