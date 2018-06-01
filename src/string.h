d01ec8ad8d0625168348ad2b8448dc1f75b1d1fa
Alexander <32514590+Scrubi19@users.noreply.github.com>
2018-06-01 09:33:49 +0600

    Delete unused prototype function from string.h

--- src/string.h
+++ src/string.h
@@ -16,33 +16,4 @@ int s_cmp(char *str1, char *str2);
 
 int str_chr(char *str, char *ch);
 int str_tok(char *str, char *delim, char **ptr);
-
-//void stok_free(char** arr);
-
-//char** stok(char* str, char delim);
-
-//void stok_print(char** arr);
-
-//char *toLower(char *str);
-
-//char toLowCase(char ch);
-
-//int isLowCase(char ch);
-
-//int isDigit(char ch);
-
-//int isApperCase(char ch);
-
-//int sequal(char *s1, char *s2);
-
-//void suntok(char *str, char delim, char *ptr[], int cnt);
-
-//int sstr(char *text, char *pattern);
-
-//int sspn(char *str, char *sym);
-
-//int s_str(char *txt, char *p);
-
-//void concat(char *dest, char* str);
-
 #endif
